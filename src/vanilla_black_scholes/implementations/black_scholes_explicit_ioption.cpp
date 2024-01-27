4ccd837672b5695ffd5bae1cfc0544d90d805c3f
Rylan Greer <rylangreer@MacBook-Pro.local>
2024-01-27 17:14:27 +0000

    work in basic and hacked initial pde solve, looks to be correct

---
+++ src/vanilla_black_scholes/implementations/black_scholes_explicit_ioption.cpp
@@ -0,0 +1,82 @@
+#include "black_scholes_explicit_ioption.hpp"
+#include <cmath>
+BlackScholesExplicit_I_Option::BlackScholesExplicit_I_Option(OptDouble p_K, OptDouble p_vol, OptDouble p_r, OptDouble p_ttm)
+: m_K(p_K), m_vol(p_vol), m_r(p_r), m_ttm(p_ttm)
+{
+    m_max_price = 3. * m_K;
+    m_ds = (m_max_price - m_min_price) / (double)((m_N_stock_points - 1));
+    m_ds_sq = m_ds * m_ds;
+    m_dt = p_ttm / (double)m_N_timesteps;
+    m_var = m_vol * m_vol;
+    calculate_scalars();
+    initialize_terminal_stock_array();
+    solve_pde();
+}
+
+void BlackScholesExplicit_I_Option::calculate_scalars(){}  
+
+OptDouble BlackScholesExplicit_I_Option::calculate_ci_term(int p_i)
+{
+    OptDouble scalar = (1. - m_r * m_dt);
+    scalar -= (m_var * m_dt / m_ds_sq) * m_stock_sq_array[p_i];
+    return scalar * m_calc_array[p_i];
+}
+
+OptDouble BlackScholesExplicit_I_Option::calculate_cip1_term(int p_i)
+{
+    OptDouble scalar = (m_r * m_dt / (2. * m_ds)) * m_stock_array[p_i];
+    scalar += 0.5 * m_var * (m_dt / m_ds_sq) * m_stock_sq_array[p_i];
+    return scalar * m_calc_array[p_i+1];
+ }
+
+OptDouble BlackScholesExplicit_I_Option::calculate_cim1_term(int p_i)
+{
+    OptDouble scalar = 0.5 * m_var * (m_dt / m_ds_sq) * m_stock_sq_array[p_i];
+    scalar -= m_r * m_dt / (2. * m_ds) * m_stock_array[p_i];
+    return scalar * m_calc_array[p_i-1];
+}
+
+OptDouble BlackScholesExplicit_I_Option::calc_opt_price_from_theta(int p_i)
+{
+    return (
+        calculate_ci_term(p_i) + calculate_cim1_term(p_i) + calculate_cip1_term(p_i)
+    );
+}
+
+void BlackScholesExplicit_I_Option::initialize_calculation_array()
+{
+    std::vector<OptDouble> v(m_N_stock_points, 0);
+    std::transform(m_stock_array.begin(), m_stock_array.end(), v.begin(),
+        [this] ( OptDouble x ){ return std::max<OptDouble>(x - this->m_K, 0. ); });
+    m_calc_array = v;
+}
+
+void BlackScholesExplicit_I_Option::evolve_calc_array_backwards(int p_timestep)
+{
+    std::vector<OptDouble> new_calc_array(m_calc_array.size());
+    for( int i(1); i < m_calc_array.size()-1; ++i )
+        new_calc_array[i] = calc_opt_price_from_theta(i);
+    new_calc_array[0] = 0.;
+    new_calc_array[new_calc_array.size() - 1] = m_max_price * exp(-m_r * m_dt * p_timestep);
+    m_calc_array = new_calc_array;
+}
+
+void BlackScholesExplicit_I_Option::solve_pde()
+{
+    initialize_calculation_array();
+    for( int i(0); i < m_N_timesteps; ++i )
+        evolve_calc_array_backwards(i);
+}
+
+void BlackScholesExplicit_I_Option::initialize_terminal_stock_array()
+{
+    std::vector<OptDouble> v(m_N_stock_points, 0);
+    std::vector<OptDouble> vsq(m_N_stock_points, 0);
+
+    std::transform(v.begin(), v.end() - 1, v.begin() + 1,
+        [this] ( OptDouble x ){ return x + this->m_ds; });
+    std::transform(v.begin(), v.end(), vsq.begin(), 
+    []( double x ){ return x * x; });
+    m_stock_array = v;
+    m_stock_sq_array = vsq;
+}
