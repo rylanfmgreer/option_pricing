64c0b9e0fc37e947e2c202d30a7f814cf30cac1e
Rylan Greer <rylangreer@MacBook-Pro.local>
2024-01-28 15:36:51 +0000

    refactor gsl utils into a separate file

--- src/utilities/gsl_interface/gsl_interface.hpp
+++ src/utilities/gsl_interface/gsl_interface.hpp
@@ -8,9 +8,13 @@ namespace Utils
 {
     auto del_GslUP = [](gsl_vector* p) { gsl_vector_free(p); };
     typedef std::unique_ptr<gsl_vector, decltype(del_GslUP) > GslUP;
+
     GslUP create_gsl_vector_of_size_n(int n);
+
     GslUP create_gsl_vector_from_std_vector( const std::vector<OptDouble>& p_v);
+
     std::vector<OptDouble> create_std_vector_from_gsl_vector(const GslUP& p_p);
+    
     std::vector<OptDouble> solve_tridiagonal_system(
         const std::vector<OptDouble> p_subdiagonal,
         const std::vector<OptDouble> p_diagonal,
--- src/utilities/utilites.cpp
+++ src/utilities/utilites.cpp
@@ -24,37 +24,6 @@ namespace Utils
         return boost::math::cdf(*p_dist, p_z);
     }
 
-    GslUP create_gsl_vector_from_std_vector( const std::vector<OptDouble>& p_v)
-    {
-        GslUP vp( gsl_vector_alloc(p_v.size() ), del_GslUP);
-        for(int i(0); i < p_v.size(); ++i )
-            gsl_vector_set(vp.get(), i, p_v[i]);
-        return vp;
-    }
-    std::vector<OptDouble> create_std_vector_from_gsl_vector(const GslUP& p_p)
-    {
-        int n = p_p->size;
-        std::vector<OptDouble> v(n);
-        for( int i(0); i < n; ++i  )
-            v[i] = gsl_vector_get(p_p.get(), i);
-        return v;
-    }
     
-    std::vector<OptDouble> solve_tridiagonal_system(
-        const std::vector<OptDouble> p_subdiagonal,
-        const std::vector<OptDouble> p_diagonal,
-        const std::vector<OptDouble> p_superdiagonal, 
-        const std::vector<OptDouble> p_b)
-    {
-        GslUP subdiagonal = create_gsl_vector_from_std_vector(p_subdiagonal);
-        GslUP diagonal = create_gsl_vector_from_std_vector(p_diagonal);
-        GslUP superdiagonal = create_gsl_vector_from_std_vector(p_superdiagonal);
-        GslUP b = create_gsl_vector_from_std_vector(p_b);
-        GslUP x( gsl_vector_alloc(p_diagonal.size()), del_GslUP);
-        gsl_linalg_solve_tridiag(diagonal.get(), superdiagonal.get(),
-            subdiagonal.get(), b.get(), x.get());
-        std::vector<OptDouble> v_x = create_std_vector_from_gsl_vector(x);
-        return v_x;
-    }
 
 };
